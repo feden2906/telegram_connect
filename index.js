a5d7db5750de84beee86ad89983cff4f1e6fef85
feden2906 <feden2906@gmail.com>
2021-11-28 23:50:40 +0200

    add env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { MTProto } = require('telegram-mtproto');
-
+require('dotenv');
 const inquirer = require('inquirer')
 const question = {
   type: 'input',
@@ -7,31 +7,32 @@ const question = {
   message: 'Enter your 2FA code',
 }
 
-const phone = {
-  num:   , // basically it is your phone number
+const config = {
+  number: process.env.PHONE,                              // basically it is your phone number
+  api_id: process.env.API_ID,                             // obtain your api_id from telegram
+  api_hash: process.env.API_HASH                          // obtain api_hash from telegram
 }
 
 const client = MTProto({})
 
 async function connect() {
   const { phone_code_hash } = await client('auth.sendCode', {
-    phone_number: phone.num,
+    phone_number: config.number,
     current_number: false,
-    api_id:  , // obtain your api_id from telegram
-    api_hash:  // obtain api_hash from telegram
+    api_id: config.api_id,
+    api_hash: config.api_hash
   })
-  const { twoFA } = await inquirer.prompt(question);   // type your 2FA code in console
+  const { twoFA } = await inquirer.prompt(question);      // type your 2FA code in console
 
   const { user } = await client('auth.signIn', {
-    phone_number: phone.num,
+    phone_number: config.number,
     phone_code_hash: phone_code_hash,
     phone_code: twoFA
   })
 
-  console.log(client.on('dialogFilter', data => console.log(data)));
-  console.log(client.on('messages.getDialogFilters', data => console.log(data)));
-  console.log(client.on('updates', data => console.log(data)));
+  // console.log(client.on('dialogFilter', data => console.log(data)));
+  // console.log(client.on('messages.getDialogFilters', data => console.log(data)));
+  // console.log(client.on('updates', data => console.log(data)));
 }
 
 connect();
-
