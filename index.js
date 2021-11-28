a00fc8d135e8cd176313a778a8cae9abd2164a4a
feden2906 <feden2906@gmail.com>
2021-11-28 23:54:20 +0200

    finish connect

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const { MTProto } = require('telegram-mtproto');
+const inquirer = require('inquirer');
 require('dotenv');
-const inquirer = require('inquirer')
+
 const question = {
   type: 'input',
   name: 'twoFA',
-  message: 'Enter your 2FA code',
-}
+  message: 'Enter your 2FA code'
+};
 
 const config = {
   number: process.env.PHONE,                              // basically it is your phone number
   api_id: process.env.API_ID,                             // obtain your api_id from telegram
   api_hash: process.env.API_HASH                          // obtain api_hash from telegram
-}
+};
 
-const client = MTProto({})
+const client = MTProto({});
 
 async function connect() {
   const { phone_code_hash } = await client('auth.sendCode', {
@@ -21,14 +22,15 @@ async function connect() {
     current_number: false,
     api_id: config.api_id,
     api_hash: config.api_hash
-  })
+  });
+
   const { twoFA } = await inquirer.prompt(question);      // type your 2FA code in console
 
-  const { user } = await client('auth.signIn', {
+  const { user } = await client('auth.signIn', {          // return user data
     phone_number: config.number,
     phone_code_hash: phone_code_hash,
     phone_code: twoFA
-  })
+  });
 
   // console.log(client.on('dialogFilter', data => console.log(data)));
   // console.log(client.on('messages.getDialogFilters', data => console.log(data)));
